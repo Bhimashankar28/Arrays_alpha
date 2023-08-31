61741453bb61b32da4df05fa1a1180aa47a24436
Bhimashankar Mathapati <115486597+Bhimashankar28@users.noreply.github.com>
2023-08-31 23:04:47 +0530

    Add files via upload

---
+++ b_reverse_array.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class b_reverse_array
+{
+    public static void reverse(int numbers[])
+    {
+        int first=0, last=numbers.length-1;
+        while(first < last)
+        {
+            int temp=numbers[last];
+            numbers[last]=numbers[first];
+            numbers[first]=temp;
+
+            first ++;
+            last--;
+        }
+
+    }
+    public static void main(String args[]) {
+        int numbers[] = {1, 2, 3, 4, 5};
+         reverse(numbers);
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
